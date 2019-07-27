489ddb0f82ba3b61b2a791a613e749e2eda23daa
moonggi <heartboy08@nate.com>
2019-07-28 02:45:17 +0900

    test2

---
+++ testgit/test22.hpp
@@ -0,0 +1,14 @@
+//
+//  test22.hpp
+//  testgit
+//
+//  Created by 프리텔레콤 on 28/07/2019.
+//  Copyright © 2019 프리텔레콤. All rights reserved.
+//
+
+#ifndef test22_hpp
+#define test22_hpp
+
+#include <stdio.h>
+
+#endif /* test22_hpp */
